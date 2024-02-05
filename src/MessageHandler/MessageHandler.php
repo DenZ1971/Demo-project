b4f3cfafc366fde46059268f1d49928acba8773c
DenZ1971 <d.zapekin@gmail.com>
2024-02-05 16:05:06 +0100

    fix mpdf

--- src/MessageHandler/MessageHandler.php
+++ src/MessageHandler/MessageHandler.php
@@ -4,12 +4,11 @@ namespace App\MessageHandler;
 
 use App\Message\PageMessage;
 use Mpdf\MpdfException;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use App\Repository\ApplicationRepository;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Mailer\MailerInterface;
-
-
 use Mpdf\Mpdf;
 
 
@@ -26,27 +25,39 @@ class MessageHandler
     }
 
 
+    /**
+     * @throws MpdfException
+     * @throws TransportExceptionInterface
+     */
     public function __invoke(PageMessage $message): void
     {
         $pageId = $message->getPage();
 
-        $page = $this->applicationRepository->find($pageId);
+
+        $page_title = $this->applicationRepository->find($pageId)->getTitle();
+        $page_description = $this->applicationRepository->find($pageId)->getDescription();
+        $page_status = $this->applicationRepository->find($pageId)->getStatusAsString();
+        $page_category = $this->applicationRepository->find($pageId)->getCategory()->getCategory();
 
 
-//        $mpdf = new Mpdf();
-//        $content = '<h1>Hello world</h1>'; //$page;
-//        $mpdf->writeHTML($content);
-//        $applicationPdf = $mpdf->output('', 'S');
-        
-        
+        $mpdf = new Mpdf();
+        $content = (sprintf("<h1>Application confirmation</h1><br>
+        <h2>Title : %s</h2><br>
+        <h3>Category : %s</h3><br>
+        <h4>Description : %s</h4><br>
+        <h3>Status : %s</h3>", $page_title, $page_category, $page_description, $page_status));
+        $mpdf->writeHTML($content);
+        $applicationPdf = $mpdf->output('', 'S');
+
+
         $email = (new Email())
             ->from('d.zapekin@gmail.com')
             ->to('d.zapekin@gmail.com') //($page->getCreateByUser()->getEmail())
             ->subject('Application confirmation')
-            ->text("Here is your application confirmation $pageId");
-            // ->attach($applicationPdf, 'application.pdf');
+            ->text("Here is your application confirmation $pageId")
+            ->attach($applicationPdf, 'application.pdf');
 
         $this->mailer->send($email);
-        
     }
-} 
+}
+
